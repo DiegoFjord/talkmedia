87ca7724c41228c895a18c6be30c6cfb5aab2edd
DiegoFjord <138102450+DiegoFjord@users.noreply.github.com>
2023-10-05 00:17:26 -0700

    Update supabaseClient.js

--- react_talk/config/supabaseClient.js
+++ react_talk/config/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 
-const supabaseUrl = 'https://gukztedupozanptkrcss.supabase.co' //process.env.REACT_APP_SUPABASE_URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1a3p0ZWR1cG96YW5wdGtyY3NzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE0NDQ5MjAsImV4cCI6MjAwNzAyMDkyMH0.spyQYD2luDU09LuVZZwvLlaSJM_XuZOmJZE83WLt3Hw' //process.env.REACT_APP_ANON_KEY
+const supabaseUrl = '' //process.env.REACT_APP_SUPABASE_URL
+const supabaseKey = '' //process.env.REACT_APP_ANON_KEY
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
